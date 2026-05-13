79ba6d76c8cce476e489c15dd12fdecbe67e0278
DREAMBOAT <dreamboat@DREAMBOATs-MacBook-Air.local>
2026-05-14 00:35:54 +0530

    fix: handle react routing refresh issue

--- app.js
+++ app.js
@@ -72,9 +72,9 @@ const sessionOptions = {
     },
 };
 
-// app.get("/", (req, res) =>{
-//     res.send("hi, i am root");
-// });
+app.get("/", (req, res) =>{                 //default route
+    res.redirect("/listings");
+});
 
 app.use(session(sessionOptions));
 app.use(flash());
